c53647e39738343a624b5a772b8ab3ec4f137917
B-bater26 <baibaxter16@gmail.com>
2026-09-01 15:33:27 -0400

    Mock up card; Add Image and Text

--- Week2/BusinessCard/App.js
+++ Week2/BusinessCard/App.js
@@ -1,16 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, Image} from 'react-native';
 
 export default function App() {
   return (
     <>
-      <StatusBar style="auto" />
+      <StatusBar style="dark" />
       <SafeAreaView style={styles.root}>
-        <View style={styles.view1}>
-          <Text>Business Card</Text>
+        <View style={styles.imageContainer}>
+          <Image
+            style={styles.image}
+            source={require('./assets/images/image_bailey.png')}
+          />
         </View>
-        <View style={styles.view2}>
-          <Text>Subtext</Text>
+        <View style={styles.textContainer}>
+          <Text>
+            Bailey Baxter
+          </Text>
+          <Text>
+            baileysprojectsonline@gmail.com
+          </Text>
+          <Text>
+            843-582-7151
+          </Text>
+          <Text>
+            https://github.com/B-bater26
+          </Text>
         </View>
       </SafeAreaView>
     </>
@@ -24,16 +38,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#b0e0e6'
   },
-  view1: {
-    flex: 1,
-    justifyContent: 'center',
+  imageContainer: {
+    flex: 2,
+    paddingTop: 150,
+    width: '100%',
     alignItems: 'center',
-    backgroundColor: 'blue'
+    justifyContent: 'center'
   },
-  view2: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'lightgray'
+  image: {
+    width: "100%",
+    height: 300,
+    resizeMode: 'cover',
+    borderColor: 'black',
+    borderWidth: 5
   }
 });
