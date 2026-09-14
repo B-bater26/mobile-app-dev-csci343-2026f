247bd92bca65dad411b66fddb3677749d3a5f126
B-bater26 <baibaxter16@gmail.com>
2026-09-14 18:11:55 -0400

    Add image state variable

--- Week4/TopMovies/App.js
+++ Week4/TopMovies/App.js
@@ -2,6 +2,33 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [restaurantItems, setRestaurantItems] = useState([
+    {
+      name: "Buffalo Wild Wings",
+      image: require("./assets/images/bww.jpg"),
+      rating: "8.7",
+    },
+    {
+      name: "Moe's Southwest Grill",
+      image: require("./assets/images/moes.jpg"),
+      rating: "8.5",
+    },
+    {
+      name: "Gino's New York Pizza",
+      image: require("./assets/images/ginos.jpg"),
+      rating: "8",
+    },
+    {
+      name: "Yamato's Japanese Steakhouse",
+      image: require("./assets/images/yamatos.jpg"),
+      rating: "7.5",
+    },
+    {
+      name: "Five Guys:\n Burgers and Fries",
+      image: require("./assets/images/fiveguys.jpg"),
+      rating: "7",
+    },
+  ]);
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
